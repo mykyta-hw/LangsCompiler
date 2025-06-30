ff5ee3ad1272c349a3cfa8c33afc51ba643aaedd
mykyta-hw <mykyta.kohut.9@gmail.com>
2025-06-30 17:23:32 +0200

    Update Enume

--- DataTypes/Enume/IRParserStates.cs
+++ DataTypes/Enume/IRParserStates.cs
@@ -1,1 +1,13 @@
-
+namespace LC.DataTypes
+{
+    public static class IRParserStates
+    {
+        public const int START = -2;
+        public const int FIRST_WORD = -1;
+        public const int LANG_WORD = 0;
+        public const int LANG_SPACE = 1;
+        public const int DEFAULT = -4;
+        public const int SECOND_WORD = 2;
+        public const int NAMESPACE = -3;
+    }
+}
